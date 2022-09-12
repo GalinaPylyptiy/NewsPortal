c270818f7c42d52d1c1a601edb35ce21e85a3b92
Galina Pylyptiy <galina_pylyptiy@mail.ru>
2022-09-12 14:29:35 +0600

    First commit

---
+++ src/main/java/com/epam/newsPortal/config/HibernateConfig.java
@@ -0,0 +1,54 @@
+package com.epam.newsPortal.config;
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@EnableTransactionManagement
+
+public class HibernateConfig {
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setDataSource(getDataSource());
+        factoryBean.setHibernateProperties(setHibernateProperties());
+        //factoryBean.setPackagesToScan("com.epam.newsPortal.entity.");
+        factoryBean.setAnnotatedClasses(com.epam.newsPortal.entity.Author.class,com.epam.newsPortal.entity.Book.class );
+        return factoryBean;
+    }
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
+    @Bean
+    public static DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/testHibernate");
+        dataSource.setUsername("galina");
+        dataSource.setPassword("u1zazgtf");
+        return dataSource;
+    }
+    private static Properties setHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.connection.characterEncoding", "utf8");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.show_sql", "true");
+        return properties;
+    }
+
+
+
+}
